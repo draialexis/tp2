f75d67c9f708eb10b7670c176579d96ed258aec6
Alexis DRAI <drai.alexis@gmail.com>
2021-02-05 12:12:36 +0100

    Add ex2

--- main.c
+++ main.c
@@ -83,7 +83,24 @@ int diagonaleDecroissante(int n) // Ecrit les entiers de 0 à n en formant une d
     return nbEspaces;
 }
 
+//ex2
+float perimetreRectangle(float longueur, float largeur) {
+    float perimetre;
+    perimetre = (2 * longueur) + (2 * largeur);
+    return perimetre;
+}
 
+float aireRectangle(float longueur, float largeur) {
+    float aire;
+    aire = longueur * largeur;
+    return aire;
+}
+
+float aireTriangleRectangle(float longueur, float largeur) {
+    float aire;
+    aire = longueur * largeur / 2;
+    return aire;
+}
 
 int main() {
 
@@ -99,6 +116,20 @@ int main() {
 
     printf("Fin du programme principal.\n");
 
+    //ex2
+    float L, l;
+    float resultat;
+
+    printf("Longueur du rectangle:");
+    scanf("%f", &L);
+    printf("Largeur du rectangle:");
+    scanf("%f", &l);
+
+    resultat = perimetreRectangle(L, l);
+    printf("Le perimetre du rectangle ayant comme longueur %f et comme largeur %f est: %f.\n", L, l, resultat);
+    printf("L'aire de ce rectangle est: %f.\n", aireRectangle(L, l));
+    resultat = aireTriangleRectangle(L, l);
+    printf("L'aire du triangle rectangle ayant comme longueur %f et comme largeur %f est: %f.\n", L, l, resultat);
 
     return 0;
 }
