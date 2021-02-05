3d4ca5d72dcdd30147001096020efc7ca96a49e3
Alexis DRAI <drai.alexis@gmail.com>
2021-02-05 12:19:57 +0100

    Add ex9, flush stdin after scanf in ex5

--- main.c
+++ main.c
@@ -133,6 +133,75 @@ void readIntegersWithWhile(int n) {
     }
 }
 
+//ex9
+void xLine(int n) {
+    printf(". . . S T A R T I N G      X  L I N E. . .\n");
+    for (int i = 0; i < n; i++) {
+        printf("x");
+    }
+    printf("\n\n");
+}
+
+void xSquare(int n) {
+    printf(". . . S T A R T I N G      X  S Q U A R E. . .\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("x");
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+void xTriangle1(int n) {
+    printf(". . . S T A R T I N G      X  T R I A N G L E 1. . .\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i + 1; j++) {
+            printf("x");
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+void xTriangle2(int n) {
+    printf(". . . S T A R T I N G      X  T R I A N G L E 2. . .\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - i; j++) {
+            printf("x");
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+void xTriangle3(int n) {
+    printf(". . . S T A R T I N G      X  T R I A N G L E 3. . .\n");
+    for (int i = 0; i < n; i++) {
+        for (int k = 0; k < i; k++) {
+            printf(" ");
+        }
+        for (int j = 0; j < n - i; j++) {
+            printf("x");
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+void xTriangle4(int n) {
+    printf(". . . S T A R T I N G      X  T R I A N G L E 4. . .\n");
+    for (int i = 0; i < n; i++) {
+        for (int k = 0; k < n - (i + 1); k++) {
+            printf(" ");
+        }
+        for (int j = 0; j < i + 1; j++) {
+            printf("x");
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
 
 int main() {
 
@@ -184,6 +253,24 @@ int main() {
     int n5;
     printf("Veuillez entrer un entier:\n");
     scanf("%d", &n5);
+    fflush(stdin);
     readIntegersWithWhile(n5);
+
+    //ex9
+    int input;
+    printf("Veuillez entrer un entier superieur a un :\n");
+    scanf("%d", &input);
+    fflush(stdin);
+    if (input > 1 && input < 130){
+        xLine(input);
+        xSquare(input);
+        xTriangle1(input);
+        xTriangle2(input);
+        xTriangle3(input);
+        xTriangle4(input);
+    } else {
+        printf("INVALID INPUT\n");
+    }
+
     return 0;
 }
